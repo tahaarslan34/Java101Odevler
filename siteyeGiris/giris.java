cbe7b0ef090ab661d6691ba155d7573d0fa83fed
Taha Arslan <taharslan25@hotmail.com>
2022-07-16 15:39:26 +0300

    7.odev basit siteye giris uygulamasi

---
+++ siteyeGiris/giris.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class giris {
+    public static void main(String[] args) {
+        String userName,password,newPassword;
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Kullanici adinizi giriniz...:");
+        userName=input.nextLine();
+
+        System.out.println("Şifrenizi giriniz...:");
+        password=input.nextLine();
+
+        if(userName.equals("Patika")){
+            if(password.equals("java123")){
+                System.out.println("Başarılı bir şekilde giriş yaptınız.");
+            }else  {
+                System.out.println("Şifreniz yanlış. Şifrenizi yeniden sıfırlayabilirsiniz.\nYeni şifrenizi giriniz.");
+                newPassword=input.nextLine();
+                if(newPassword.equals("java123"))
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                    else{
+                        System.out.println("Şifre oluşturuldu.");
+                    }
+                
+
+            }
+        } else {
+            System.out.println("Bilgileriniz yanlış.");
+        }
+    }
+}
